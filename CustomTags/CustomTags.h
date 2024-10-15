9dc8fdc5cfcab262e49dbb2b5f61138a5b4dbb06
Ver <aloss8411@gmail.com>
2024-10-15 16:48:45 +0800

    Initial Commit

---
+++ CustomTags/CustomTags.h
@@ -0,0 +1,18 @@
+//
+//  CustomTags.h
+//  CustomTags
+//
+//  Created by Wei Ran Wang on 2024/10/15.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CustomTags.
+FOUNDATION_EXPORT double CustomTagsVersionNumber;
+
+//! Project version string for CustomTags.
+FOUNDATION_EXPORT const unsigned char CustomTagsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CustomTags/PublicHeader.h>
+
+
